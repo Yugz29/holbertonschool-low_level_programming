b1dc1845112ca05e34ac03029882e7a56dcd2e72
Yugz29 <yannduzelier@gmail.com>
2025-06-17 07:57:11 +0000

    Task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main : It prints "Programming is like building a multilingual puzzle
+ * main - It prints "Programming is like building a multilingual puzzle
  *
- * Return : Always returns 0
+ * Return: Always returns 0
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
