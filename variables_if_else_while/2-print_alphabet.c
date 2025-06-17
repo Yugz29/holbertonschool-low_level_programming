66e6dfd72177f24e6a1157dfa0da999c1f5825b2
Yugz29 <yannduzelier@gmail.com>
2025-06-17 16:14:29 +0000

    Task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: always return 0
+ *
+ **/
+
+int main(void)
+{
+	char lettre = 'a';
+
+	while (lettre <= 'z')
+	{
+		putchar(lettre);
+		lettre++;
+	}
+
+	return (0);
+}
