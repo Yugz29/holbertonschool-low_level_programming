621f921b326b6c8a99a83d7270388c1d9ec794c7
Yugz29 <yannduzelier@gmail.com>
2025-06-22 16:59:04 +0000

    Task 10

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of two digits
+ *
+ * Return: always return 0
+ *
+ **/
+
+int main(void)
+{
+	int first = 0;
+	int second;
+
+	while (first < 9)
+	{
+		second = first + 1;
+
+		while (second <= 9)
+		{
+			putchar(first + '0');
+			putchar(second + '0');
+
+			if (!(first == 8 && second == 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			second++;
+		}
+		first++;
+	}
+	return (0);
+}
