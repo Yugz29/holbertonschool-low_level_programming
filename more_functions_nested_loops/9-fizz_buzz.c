a76f2a5083f983fea7e3b886ac43af39f92efa22
Yugz29 <yannduzelier@gmail.com>
2025-06-24 14:20:45 +0000

    Task 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers form 1 to 0
+ *
+ * Return: always return 0
+ **/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d", i);
+
+			if (i != 100)
+			{
+				printf(" ");
+			}
+		}
+	}
+	printf("\n");
+	return (0);
+}
