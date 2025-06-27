057835b4fba486b24b00a1dfa7ed9c2171c058db
Yugz29 <yannduzelier@gmail.com>
2025-06-27 14:32:50 +0000

    Task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ *
+ * @s: my string
+**/
+
+void rev_string(char *s)
+{
+	char temp;
+	int start = 0;
+	int end;
+	int len = 0;
+
+	while (s[len] != ('\0'))
+	{
+		len++;
+	}
+
+	end = len - 1;
+
+	while (start != end + 1)
+	{
+		temp = s[end];
+		s[end] = s[start];
+		s[start] = temp;
+		start++;
+		end--;
+	}
+}
