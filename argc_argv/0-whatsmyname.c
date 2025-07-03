a319979723270e8aea161f154f3599651aab229b
Yugz29 <yannduzelier@gmail.com>
2025-07-03 08:24:01 +0000

    Task 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point of the program
+ * @argc: number of arguments
+ * @argv: array of argument strings
  *
- *
- *
- *
+ * Return: Always 0
  **/
 
 int main(int argc, char *argv[])
 {
 	(void)argc;
 
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
