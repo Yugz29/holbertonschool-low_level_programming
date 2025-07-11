7a7a5db75f51acfed2be372137475e37a5defcfb
Yugz29 <yannduzelier@gmail.com>
2025-07-11 17:19:46 +0000

    fcking Task 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -19,9 +19,14 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", d->name);
 	}
-
-	printf("Age: %f\n", d->age);
-
+	if (d->age == 0)
+	{
+		printf("Age: (nil)\n");
+	}
+	else
+	{
+		printf("Age: %f\n", d->age);
+	}
 	if (d->owner == NULL)
 	{
 		printf("Owner: (nil)\n");
