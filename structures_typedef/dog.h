f2bad2d68988088b62c0418507bdaebe5ee2e52f
Yugz29 <yannduzelier@gmail.com>
2025-07-11 10:31:58 +0000

    Task 0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struc dog - structure to class different info about dog
+ *
+ * name: name of the dog
+ * age: age of the dog
+ * owner: owner of the dog
+ *
+ **/
+
 typedef struct dog {
 	char* name;
 	float age;
