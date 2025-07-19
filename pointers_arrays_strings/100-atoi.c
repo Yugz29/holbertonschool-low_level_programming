199b95b1cab315ebac23f41bf7ea4a044e21299c
Yugz29 <yannduzelier@gmail.com>
2025-07-19 12:29:55 +0200

    Task 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: The string to be converted
+ *
+ * Return: The integer representation of the string
+ **/
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int op = 1;
+	int number = 0;
+
+	while (s[i] && (s[i] < '0' || s[i] > '9') && s[i] != '+' && s[i] != '-')
+	{
+		i++;
+	}
+	while (s[i] == '+' || s[i] == '-')
+	{
+		if (s[i] == '-')
+		{
+			op *= -1;
+		}
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		number = number * 10 + (s[i] - '0');
+		i++;
+	}
+	return (number * op);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -45,5 +45,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
+int _atoi(char *s);
 
 #endif
