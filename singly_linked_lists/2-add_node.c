afcf9cad35f25ac2168b230e9ca16265a21f7f04
Yugz29 <yannduzelier@gmail.com>
2025-07-28 15:13:04 +0200

    fix remove function strlen, write directly in add_node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -16,6 +16,8 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 
+	unsigned int len = 0;
+
 	new = malloc(sizeof(list_t));
 
 	if (!new)
@@ -28,7 +30,11 @@ list_t *add_node(list_t **head, const char *str)
 		free(new);
 		return (NULL);
 	}
-	new->len = _strlen(str);
+
+	while (str[len])
+	len++;
+
+	new->len = len;
 	new->next = *head;
 	*head = new;
 
